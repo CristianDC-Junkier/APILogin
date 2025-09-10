552db0a9e04e70fa1ea2bd42ea9d0bbe11e4fe99
Cristian Delgado Cruz <cristiandelgadoinformatica@gmail.com>
2025-09-10 08:50:22 +0200

    0.1

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+﻿const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// ----------------------
+// API (backend)
+// ----------------------
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hola desde Node.js 🚀" });
+});
+
+// ----------------------
+// FRONTEND
+// ----------------------
+/*
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+*/
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+});
