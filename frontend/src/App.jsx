b6756218a00ee4bb477c73dc5b83ce74502fb36b
informaticaalmonte <informaticaalcaldia@aytoalmonte.es>
2025-09-15 14:12:55 +0200

    logout fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
 ﻿import React from 'react';
 import './styles/Global.css';
-import './styles/theme/Dark.css';
-import './styles/theme/Light.css';
 
-import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
 
 import AppRouter from './router/AppRouter';
@@ -11,9 +8,7 @@ import AppRouter from './router/AppRouter';
 const App = () => {
     return (
         <AuthProvider>
-            <ThemeProvider>
-                <AppRouter />
-            </ThemeProvider>
+            <AppRouter />
         </AuthProvider>
     );
 };
