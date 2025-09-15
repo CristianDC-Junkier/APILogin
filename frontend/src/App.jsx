6b3105da2cec04a69742172dda9d626307243e3f
Cristian Delgado Cruz <cristiandelgadoinformatica@gmail.com>
2025-09-15 14:14:05 +0200

    v0.9.1 documentando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 ﻿import React from 'react';
 import './styles/Global.css';
-import './styles/theme/Dark.css';
-import './styles/theme/Light.css';
 
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
