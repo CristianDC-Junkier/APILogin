88816d4a5c394b26fc659671466ca2b657d47c50
Cristian Delgado Cruz <cristiandelgadoinformatica@gmail.com>
2025-10-02 09:49:00 +0200

    v1.1.0 Backend Middlewares Models Routes Server Changes Añadido Link Department

---
+++ backend/routes/LinksRoutes.js
@@ -0,0 +1,31 @@
+﻿
+const express = require("express");
+const router = express.Router();
+
+const LinksController = require("../controllers/LinksController");
+const { adminOnly, isAuthenticated } = require("../middlewares/Auth");
+
+/**
+ * Rutas para gestión de links.
+ *
+ * Endpoints:
+ * - GET    /                → Listar todos los links (solo ADMIN o SUPERADMIN).
+ * - POST   /                → Crear un nuevo link (solo ADMIN o SUPERADMIN).
+ * - PUT    /:id             → Actualizar datos de un link por ID (solo ADMIN o SUPERADMIN).
+ * - DELETE /:id             → Eliminar un link por ID (solo ADMIN o SUPERADMIN).
+ * - GET    /department/:id  → Obtener Links por departamento (Solo Usuarios identificados).
+ * 
+ * Middleware:
+ * - `adminOnly` → Restringe el acceso a usuarios con roles de administrador.
+ * - `isAuthenticated` → Restringe el acceso a usuarios autenticados.
+ */
+
+
+router.get("/", adminOnly, LinksController.list);
+router.post("/", adminOnly, LinksController.create);
+router.put("/:id", adminOnly, LinksController.update);
+router.delete("/:id", adminOnly, LinksController.delete);
+
+router.get("/department/:id", isAuthenticated, LinksController.getLinksByDepartment);
+
+module.exports = router;
