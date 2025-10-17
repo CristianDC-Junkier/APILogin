6a4508dc12662493d25590f4a5d6c13078995739
informaticaalmonte <informaticaalcaldia@aytoalmonte.es>
2025-10-17 13:28:31 +0200

    asd

--- backend/controllers/UserAccountController.js
+++ backend/controllers/UserAccountController.js
@@ -352,6 +352,7 @@ class UserAccountController {
     */
     static async deleteMyAccount(req, res) {
         try {
+            console.log(req.user);
             const { id } = req.user.id;
             const { version } = req.query;
 
